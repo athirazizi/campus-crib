5c077a6c8536e0e67cfcde0fadf695fec38dc316
athirazizi <aziziathir@gmail.com>
2023-03-13 19:16:57 +0000

    added tabbed page

--- CampusCrib/MainPage.xaml.cs
+++ CampusCrib/MainPage.xaml.cs
@@ -5,14 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace CampusCrib
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : Xamarin.Forms.TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
+            // prevent user from swiping between tabbed pages
+            this.On<Xamarin.Forms.PlatformConfiguration.Android>().SetIsSwipePagingEnabled(false);
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            // prevent user from going back to login page
+            return true;
+        }
+
+        private void tbLogout_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
         }
     }
 }
