ae6339ee0919ccad861fd6d991eb83bd85e3f6d6
athirazizi <aziziathir@gmail.com>
2023-03-13 21:38:14 +0000

    added icons

--- CampusCrib.Android/MainActivity.cs
+++ CampusCrib.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace CampusCrib.Droid
 {
-    [Activity(Label = "CampusCrib", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "CampusCrib", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
