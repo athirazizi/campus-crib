45882261bfa51f981212170e6c975bf2315fdd08
athirazizi <aziziathir@gmail.com>
2023-03-13 16:38:43 +0000

    changed status bar colour to black

--- CampusCrib.Android/MainActivity.cs
+++ CampusCrib.Android/MainActivity.cs
@@ -17,6 +17,7 @@ namespace CampusCrib.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 0, 0, 0));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
